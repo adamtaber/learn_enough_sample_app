7296dddcff532e3ac8609a4aab11f75f655e0b44
Adam Taber <altaber@gmail.com>
2022-10-24 16:00:15 -0500

    add user following and update feed

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,6 @@ module SampleApp
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     config.active_storage.variant_processor = :mini_magick
+    config.action_view.embed_authenticity_token_in_remote_forms = true
   end
 end
